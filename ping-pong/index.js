5cea032b0e2f87948593d57a4db6c7a949e09165
André Souza <andresouza@meraki.dev.br>
2023-07-21 21:13:44 -0300

    [UPDATE] Ping Pong finish

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -33,7 +33,7 @@ const leftPaddle = {
   w: line.w,
   h: 200,
   _move: function () {
-    this.y = mouse.y - (this.h / 2);
+    this.y = mouse.y - this.h / 2;
   },
   draw: function () {
     canvasCtx.fillStyle = "#FFF";
@@ -48,8 +48,16 @@ const rigthPaddle = {
   y: 100,
   w: line.w,
   h: 200,
-  _move: function() {
-    this.y = ball.y
+  speed: 5,
+  _move: function () {
+    if (this.y + this.h / 2 < ball.y + ball.r) {
+      this.y += this.speed;
+    } else {
+      this.y -= this.speed;
+    }
+  },
+  speedUp: function(){
+    this.speed += 2
   },
   draw: function () {
     canvasCtx.fillStyle = "#FFF";
@@ -59,8 +67,14 @@ const rigthPaddle = {
 };
 
 const score = {
-  human: 1,
-  computer: 2,
+  human: 0,
+  computer: 0,
+  _increaseHuman: function () {
+    this.human++;
+  },
+  _increaseComputer: function () {
+    this.computer++;
+  },
   draw: function () {
     canvasCtx.font = "bold 72px Arial";
     canvasCtx.textAlign = "center";
@@ -72,19 +86,70 @@ const score = {
 };
 
 const ball = {
-  x: 320,
-  y: 110,
+  x: 0,
+  y: 0,
   r: 20,
   speed: 5,
+  directionY: 1,
+  directionX: 1,
+  _calcPosition: function () {
+    if (this.x > field.w - this.r - rigthPaddle.w - gapX) {
+      if (
+        this.y + this.r > rigthPaddle.y &&
+        this.y - this.r < rigthPaddle.y + rigthPaddle.h
+      ) {
+        this._reverseX();
+      } else {
+        score._increaseHuman();
+        this._pointUp();
+      }
+    }
+
+    // Verifica se jogador 2 fez ponto.
+
+    if (this.x < this.r + leftPaddle.w + gapX) {
+      if (
+        this.y + this.r > leftPaddle.y &&
+        this.y - this.r < leftPaddle.y + leftPaddle.h
+      ) {
+        this._reverseX();
+      } else {
+        score._increaseComputer();
+        this._pointUp();
+      }
+    }
+
+    if (
+      (this.y - this.r < 0 && this.directionY < 0) ||
+      (this.y > field.h - this.r && this.directionY > 0)
+    ) {
+      this._reverseY();
+    }
+  },
+  _reverseY: function () {
+    this.directionY *= -1;
+  },
+  _reverseX: function () {
+    this.directionX *= -1;
+  },
+  _speedUp: function () {
+    this.speed += 3;
+  },
+  _pointUp: function () {
+    this.x = field.w / 2;
+    this.y = field.h / 2;
+    this._speedUp();
+  },
   _move: function () {
-    this.x += 1 * this.speed;
-    this.y += 1 * this.speed;
+    this.x += this.directionX * this.speed;
+    this.y += this.directionY * this.speed;
   },
   draw: function () {
     canvasCtx.fillStyle = "#FFF";
     canvasCtx.beginPath();
     canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
     canvasCtx.fill();
+    this._calcPosition();
     this._move();
   },
 };
