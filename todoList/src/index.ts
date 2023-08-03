a420dcadf3b29d2ff795170cc98b2745a94cac7d
André Souza <andresouza@meraki.dev.br>
2023-08-03 19:03:30 -0300

    [update] Interface e Classes

---
+++ todoList/src/index.ts
@@ -0,0 +1,89 @@
+(() => {
+
+    interface Task {
+        id: string;
+        dateCreated: Date;
+        dateUpdated: Date;
+        description: string;
+        render(): string;
+    }
+
+    class Reminder implements Task{
+        id: string = '';
+        dateCreated: Date = new Date;
+        dateUpdated: Date = new Date;
+        description: string = '';
+        
+        date: Date = new Date();
+        notifications: Array<string> = ["EMAIL"];
+
+        constructor(
+            description: string, 
+            date: Date, 
+            notifications: Array<string>
+        ) {
+           this.description = description;
+           this.date = date;
+           this.notifications = notifications; 
+        }
+
+
+        render(): string {
+            return JSON.stringify(this);
+        }
+    }
+
+    class Todo implements Task{
+        id: string = '';
+        dateCreated: Date = new Date();
+        dateUpdated: Date = new Date();
+        description: string = '';
+
+        done: boolean = false;
+        constructor(
+            description: string
+        ) {
+            this.description = description
+        }   
+
+
+        render(): string {
+            return JSON.stringify(this);
+        }
+
+    }
+
+    const todo = new Todo('Todo criado com classe.');
+
+    const reminder = new Reminder("Reminder criado com classe", new Date(), ['EMAIL']);
+
+
+    const taskView = {
+        render(tasks: Array<Task>) {
+            const tasksList = document.getElementById("tasksList");
+            while (tasksList?.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+
+            tasks.forEach(task => {
+                const li = document.createElement("li");
+                const textNode = document.createTextNode(task.render());
+                li.appendChild(textNode);
+                tasksList?.appendChild(li)
+            })
+        }
+    };
+
+    const TaskController = (view: typeof taskView) => {
+        const tasks: Array<Task> = [todo, reminder];
+
+        const handleEvent = (event: Event) => {
+            event.preventDefault();
+            view.render(tasks);
+        }
+
+
+        document.getElementById('taskForm')?.addEventListener("submit", handleEvent);
+    };
+    TaskController(taskView);
+})();
